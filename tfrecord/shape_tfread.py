642c80602d8fad3504b27507f3fc806f59e6c257
mnasiri <31705845+m-nasiri@users.noreply.github.com>
2017-11-27 18:43:56 +0330

    Update shape_tfread.py

--- tfrecord/shape_tfread.py
+++ tfrecord/shape_tfread.py
@@ -39,55 +39,55 @@ def read_and_decode(filename_queue, batch_size):
 
     return images, labels
 
-
-# reading images with batch_size = 4
-# after each fetch we will get 4 images and their labels
-batch_size1 = 4
-num_epochs1 = 1
-filename_queue1 = tf.train.string_input_producer([tfrecords_filename], num_epochs=num_epochs1)
-image1, label1 = read_and_decode(filename_queue1, batch_size=batch_size1)
-
-
-# reading images with batch_size = 8
-# after each fetch we will get 8 images and their labels
-batch_size2 = 8
-num_epochs2 = 1
-filename_queue2 = tf.train.string_input_producer([tfrecords_filename],  num_epochs=num_epochs2)
-image2, label2 = read_and_decode(filename_queue2, batch_size=batch_size2)
-
-# The op for initializing the variables.
-init_op = tf.group(tf.global_variables_initializer(),
-                   tf.local_variables_initializer())
-
-with tf.Session()  as sess:
-    
-    sess.run(init_op)
-    
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
-    
-    
-    for i in range(num_epochs1*samples_num//batch_size1):        
-        batch_x, batch_y = sess.run([image1, label1])
-        print(batch_x.shape)
-        print('current batch = '+str(i))
-        
-        for j in range(batch_size1):
-            io.imsave('./dataset_restored1/'+'batch_'+str(i)+'_image_'+str(j)+'.png', batch_x[j, :, :])
+with tf.Graph().as_default():
+    # reading images with batch_size = 4
+    # after each fetch we will get 4 images and their labels
+    batch_size1 = 4
+    num_epochs1 = 1
+    filename_queue1 = tf.train.string_input_producer([tfrecords_filename], num_epochs=num_epochs1)
+    image1, label1 = read_and_decode(filename_queue1, batch_size=batch_size1)
 
 
-    for i in range(num_epochs2*samples_num//batch_size2):        
-        batch_x, batch_y = sess.run([image2, label2])
-        print(batch_x.shape)
-        print('current batch = '+str(i))
-        
-        for j in range(batch_size2):
-            io.imsave('./dataset_restored2/'+'batch_'+str(i)+'_image_'+str(j)+'.png', batch_x[j, :, :])
+    # reading images with batch_size = 8
+    # after each fetch we will get 8 images and their labels
+    batch_size2 = 8
+    num_epochs2 = 1
+    filename_queue2 = tf.train.string_input_producer([tfrecords_filename],  num_epochs=num_epochs2)
+    image2, label2 = read_and_decode(filename_queue2, batch_size=batch_size2)
+
+    # The op for initializing the variables.
+    init_op = tf.group(tf.global_variables_initializer(),
+                       tf.local_variables_initializer())
+
+    with tf.Session()  as sess:
+
+        sess.run(init_op)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+
+        for i in range(num_epochs1*samples_num//batch_size1):        
+            batch_x, batch_y = sess.run([image1, label1])
+            print(batch_x.shape)
+            print('current batch = '+str(i))
+
+            for j in range(batch_size1):
+                io.imsave('./dataset_restored1/'+'batch_'+str(i)+'_image_'+str(j)+'.png', batch_x[j, :, :])
+
+
+        for i in range(num_epochs2*samples_num//batch_size2):        
+            batch_x, batch_y = sess.run([image2, label2])
+            print(batch_x.shape)
+            print('current batch = '+str(i))
+
+            for j in range(batch_size2):
+                io.imsave('./dataset_restored2/'+'batch_'+str(i)+'_image_'+str(j)+'.png', batch_x[j, :, :])
 
 
 
-        
-    coord.request_stop()
-    coord.join(threads)
+
+        coord.request_stop()
+        coord.join(threads)
 
 
