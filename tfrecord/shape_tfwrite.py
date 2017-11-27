9232e21961915e5b386931f3ba7d7ab5f2b40645
mnasiri <31705845+m-nasiri@users.noreply.github.com>
2017-11-27 13:49:14 +0330

    Update shape_tfwrite.py

--- tfrecord/shape_tfwrite.py
+++ tfrecord/shape_tfwrite.py
@@ -16,11 +16,16 @@ def _int64_feature(value):
 def _float32_feature(value):
     return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
-
+# make tfrecord file of test images
 dataset_squares_path = 'dataset/test/squares/*.png'
 dataset_triangles_path = 'dataset/test/triangles/*.png'
 tfrecords_filename = 'shape_test.tfrecords'
 
+
+# make tfrecord file of train images
+#dataset_squares_path = 'dataset/train/squares/*.png'
+#dataset_triangles_path = 'dataset/train/triangles/*.png'
+#tfrecords_filename = 'shape_train.tfrecords'
 
 # read addresses and labels from the 'train' folder
 squares_addrs = glob.glob(dataset_squares_path)
@@ -29,6 +34,7 @@ triangles_addrs = glob.glob(dataset_triangles_path)
 squares_len = len(squares_addrs)
 triangles_len = len(triangles_addrs)
 
+# make labels
 # [1, 0] = squares     [0, 1] = triangles
 squares_labels = np.zeros((2, squares_len), dtype=np.uint8)
 squares_labels[0,:] = 1
@@ -43,19 +49,13 @@ filename_pairs = list(zip(images_addrs, images_labels.T))
 # to shuffle data
 shuffle(filename_pairs)
 
-# Let's collect the real images to later on compare
-# to the reconstructed ones
-original_images = []
-
 writer = tf.python_io.TFRecordWriter(tfrecords_filename)
 
 for img_path, label in filename_pairs:
     
+    # in this case all images are png with (32, 32) shape
     img = np.array(Image.open(img_path))    # (32, 32) uint8
     
-    # Put in the original images into array
-    # Just for future check for correctness
-    original_images.append((img, label))
     img_raw = img.tostring()
     label_raw = label.tostring()
     
