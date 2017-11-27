277224659bea39f169c82886b541e6c4d6d0f0d0
mnasiri <31705845+m-nasiri@users.noreply.github.com>
2017-11-27 13:44:45 +0330

    Update shape_tfread.py

--- tfrecord/shape_tfread.py
+++ tfrecord/shape_tfread.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import skimage.io as io
 
+# number of all samples in test or train folders
+# in this case test folders
 samples_num = 32
-
 
 tfrecords_filename = 'shape_test.tfrecords'
 
@@ -39,13 +40,16 @@ def read_and_decode(filename_queue, batch_size):
     return images, labels
 
 
-
+# reading images with batch_size = 4
+# after each fetch we will get 4 images and their labels
 batch_size1 = 4
 num_epochs1 = 1
 filename_queue1 = tf.train.string_input_producer([tfrecords_filename], num_epochs=num_epochs1)
 image1, label1 = read_and_decode(filename_queue1, batch_size=batch_size1)
 
 
+# reading images with batch_size = 8
+# after each fetch we will get 8 images and their labels
 batch_size2 = 8
 num_epochs2 = 1
 filename_queue2 = tf.train.string_input_producer([tfrecords_filename],  num_epochs=num_epochs2)
