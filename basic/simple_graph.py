38569284dee97694e4fd89f045566f71006a997d
majid nasiri <31705845+m-nasiri@users.noreply.github.com>
2018-02-20 12:04:33 -0800

    Update simple_graph.py

--- basic/simple_graph.py
+++ basic/simple_graph.py
@@ -11,18 +11,19 @@
 import numpy as np
 import tensorflow as tf
     
-W = tf.placeholder(tf.float32, shape=[2, 10])
+# placeholders
 X = tf.placeholder(tf.float32, shape=[10, 6])
-b = tf.placeholder(tf.float32, shape=[2, 6])
 
+# variables
+W = tf.Variable(tf.random_normal([2, 10]), tf.float32)
+b = tf.Variable(tf.random_normal([2, 6]), tf.float32)
+
+# operations
 WX = tf.matmul(W, X)
 WXb = tf.add(WX, b)
 relu = tf.nn.relu(WXb)
 
-#print(WX.get_shape())
-#print(WXb.get_shape())
-#print(relu.get_shape())
- 
+# create Session
 with tf.Session() as sess:
     
     feed_dict = {W: np.random.randn(2,10),
@@ -31,7 +32,5 @@ with tf.Session() as sess:
 
     relu_val = sess.run(relu, feed_dict=feed_dict)
     
-  
-        
     
     
